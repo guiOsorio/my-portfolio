ee2aea8632ace7b5552438174e47edb3f9645de7
guiOsorio <gosorio@sandiego.edu>
2022-03-04 22:50:46 -0800

    Projects almost done

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -37,96 +37,126 @@ class Projects extends Component {
         return (
             <div className="section__projects screen-size2">
                 <h2>PROJECTS</h2>
-                <div className='projects-left'>
+                <div className='projects-top'>
                     <h3 className='projects-title'>Software / Web Development</h3>
-                    <div className='webdev-project card'>
-                        <div className='front scheduling-img'>
-                            <h4>Scheduling App</h4>
+                    <div className='projects-category'>
+                        <div className='webdev-project card'>
+                            <div className='front scheduling-img'>
+                                <h4>Scheduling App</h4>
+                            </div>
+                            <div className='back'>
+                                <p>&rarr; Technologies used: Python (Flask) + SQL (PostgreSQL) + AWS (for automated emails) <br />
+                                &rarr; Scheduling platform for tennis clubs</p>
+                                <p className='link-center'>
+                                    <a href='https://github.com/guiOsorio/Scheduling-Demo' target='_blank'>Github repository</a>
+                                </p>
+                                <p className='link-center'>
+                                    <a href='#'>Project's page</a>
+                                </p>
+                            </div>
                         </div>
-                        <div className='back'>
-                            <p>Technologies used: Python (Flask) + SQL (PostgreSQL) + AWS (for automated emails) <br />Scheduling platform for tennis clubs</p>
-                            <a href='https://github.com/guiOsorio/Scheduling-Demo' target='_blank'>Github repository</a> <br />
-                            <a href='#'>Project's page</a>
+                        <div className='webdev-project card'>
+                            <div className='front trivia-img'>
+                                <h4>CS50 C++ Trivia</h4>
+                            </div>
+                            <div className='back'>
+                                <p>&rarr; Technologies used: C++ <br />
+                                &rarr; Command-line trivia game based on Harvard's CS50 online course</p>
+                                <p className='link-center'>
+                                    <a href='https://github.com/guiOsorio/CS50-Trivia' target='_blank'>Github repository</a>
+                                </p>
+                                <p className='link-center'>
+                                    <a href='#'>Project's page</a>
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div className='webdev-project card'>
-                        <div className='front trivia-img'>
-                            <h4>CS50 C++ Trivia</h4>
+                        <div className='webdev-project card'>
+                            <div className='front acmproject-img'>
+                                <h4>ACM Website</h4>
+                            </div>
+                            <div className='back'>
+                                <p>&rarr; Technologies used: Python (Django) + JavaScript (React) <br />
+                                &rarr; Main contributor on design and implementation</p>
+                                <a href='https://github.com/usd-cs/acm-website' target='_blank'>Github repository</a> 
+                                <a href='http://usd-acm-website.herokuapp.com/' target='_blank' className='link-right'>Deployed project</a> <br/>
+                                <p className='link-center'>
+                                    <a href='#'>Project's page</a>
+                                </p>
+                            </div>
                         </div>
-                        <div className='back'>
-                            <p>Technologies used: C++ <br />Command-line trivia game based on Harvard's CS50 online course</p>
-                            <a href='https://github.com/guiOsorio/CS50-Trivia' target='_blank'>Github repository</a> <br />
-                            <a href='#'>Project's page</a>
+                        <div className='webdev-project card'>
+                            <div className='front financeproject-img'>
+                                <h4>Finance Stock Displayer</h4>
+                            </div>
+                            <div className='back'>
+                                <p>&rarr; Technologies used: Python (Flask) + SQL (SQLite) + independent API <br />
+                                &rarr; Platform to simulate stock market transactions</p>
+                                <a href='https://github.com/guiOsorio/Finance' target='_blank'>Github repository</a> 
+                                <a href='https://gui-finance.herokuapp.com/login' target='_blank' className='link-right'>Deployed project</a> <br/>
+                                <p className='link-center'>
+                                    <a href='#'>Project's page</a>
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div className='webdev-project card'>
-                        <div className='front acmproject-front'>
-                            <h4>ACM Website</h4>
+                        <div className='webdev-project card'>
+                            <div className='front recipes-img'>
+                                <h4>Recipes Platform</h4>
+                            </div>
+                            <div className='back'>
+                                <p>&rarr; Technologies used: JavaScript (React) + independent API <br />
+                                &rarr; My 1st web dev project</p>
+                                <a href='https://github.com/guiOsorio/Gui-Recipes' target='_blank'>Github repository</a> 
+                                <a href='https://gui-recipes.herokuapp.com/' target='_blank' className='link-right'>Deployed project</a> <br/>
+                                <p className='link-center'>
+                                    <a href='#'>Project's page</a>
+                                </p>
+                            </div>
                         </div>
-                        <div className='back'>
-                            <p>Technologies used: Python (Django) + JavaScript (React) <br />Main contributor on design and implementation</p>
-                            <a href='https://github.com/usd-cs/acm-website' target='_blank'>Github repository</a> <a href='http://usd-acm-website.herokuapp.com/' target='_blank'>Deployed project</a> <br/>
-                            <a href='#'>Project's page</a>
-                        </div>
-                    </div>
-                    <div className='webdev-project card'>
-                        <div className='front finance-front'>
-                            <h4>Finance Stock Displayer</h4>
-                        </div>
-                        <div className='back'>
-                            <p>Technologies used: Python (Flask) + SQL (SQLite) + independent API <br />- platform to simulate stock market transactions</p>
-                            <a href='https://github.com/guiOsorio/Finance' target='_blank'>Github repository</a> <a href='https://gui-finance.herokuapp.com/login' target='_blank'>Deployed project</a> <br/>
-                            <a href='#'>Project's page</a>
-                        </div>
-                    </div>
-                    <div className='webdev-project card'>
-                        <div className='front recipes-front'>
-                            <h4>Recipes Platform</h4>
-                        </div>
-                        <div className='back'>
-                            <p>Technologies used: JavaScript (React) + independent API <br />- my 1st web dev project</p>
-                            <a href='https://github.com/guiOsorio/Gui-Recipes' target='_blank'>Github repository</a> <a href='https://gui-recipes.herokuapp.com/' target='_blank'>Deployed project</a> <br/>
-                            <a href='#'>Project's page</a>
-                        </div>
-                    </div>
-                    <div className='webdev-project card'>
-                        <div className='front algos-front'>
-                            <h4>JS Algorithms</h4>
-                        </div>
-                        <div className='back'>
-                            <p>Technologies used: JavaScript <br />- implementation of different algorithms on a web page</p>
-                            <a href='https://github.com/guiOsorio/JS-Algos' target='_blank'>Github repository</a> <a href='https://guiosorio.github.io/JS-Algos/' target='_blank'>Deployed project</a> <br/>
-                            <a href='#'>Project's page</a>
+                        <div className='webdev-project card'>
+                            <div className='front algos-img'>
+                                <h4>JS Algorithms</h4>
+                            </div>
+                            <div className='back'>
+                                <p>&rarr; Technologies used: JavaScript <br />
+                                &rarr; Implementation of different algorithms on a web page</p>
+                                <a href='https://github.com/guiOsorio/JS-Algos' target='_blank'>Github repository</a> 
+                                <a href='https://guiosorio.github.io/JS-Algos/' target='_blank' className='link-right'>Deployed project</a> <br/>
+                                <p className='link-center'>
+                                    <a href='#'>Project's page</a>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className='projects-right'>
-                <h3 className='projects-title'>Data Analytics</h3>
-                    <div className='data-project card'>
-                        <div className='front scheduling-front'>
-                            <h4>Scheduling App</h4>
+                <div className='projects-bottom'>
+                    <h3 className='projects-title'>Data Analytics</h3>
+                    <div className='projects-category'>
+                        <div className='data-project card'>
+                            <div className='front scheduling-front'>
+                                <h4>Scheduling App</h4>
+                            </div>
+                            <div className='back'>
+                                <p>Technologies used: Python (Flask) + SQL (PostgreSQL) + AWS (for automated emails)</p>
+                                <a href='#'>Project's page</a>
+                            </div>
                         </div>
-                        <div className='back'>
-                            <p>Technologies used: Python (Flask) + SQL (PostgreSQL) + AWS (for automated emails)</p>
-                            <a href='#'>Project's page</a>
+                        <div className='data-project card'>
+                            <div className='front scheduling-front'>
+                                <h4>Scheduling App</h4>
+                            </div>
+                            <div className='back'>
+                                <p>Technologies used: Python (Flask) + SQL (PostgreSQL) + AWS (for automated emails)</p>
+                                <a href='#'>Project's page</a>
+                            </div>
                         </div>
-                    </div>
-                    <div className='data-project card'>
-                        <div className='front scheduling-front'>
-                            <h4>Scheduling App</h4>
-                        </div>
-                        <div className='back'>
-                            <p>Technologies used: Python (Flask) + SQL (PostgreSQL) + AWS (for automated emails)</p>
-                            <a href='#'>Project's page</a>
-                        </div>
-                    </div>
-                    <div className='data-project card'>
-                        <div className='front scheduling-front'>
-                            <h4>Scheduling App</h4>
-                        </div>
-                        <div className='back'>
-                            <p>Technologies used: Python (Flask) + SQL (PostgreSQL) + AWS (for automated emails)</p>
-                            <a href='#'>Project's page</a>
+                        <div className='data-project card'>
+                            <div className='front scheduling-front'>
+                                <h4>Scheduling App</h4>
+                            </div>
+                            <div className='back'>
+                                <p>Technologies used: Python (Flask) + SQL (PostgreSQL) + AWS (for automated emails)</p>
+                                <a href='#'>Project's page</a>
+                            </div>
                         </div>
                     </div>
                 </div>
