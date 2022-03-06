0e650455c218dbb6cdbd0e04aaf0546d925cf32c
guiOsorio <gosorio@sandiego.edu>
2022-03-06 10:02:08 -0800

    Education section started

--- src/Components/Education/index.js
+++ src/Components/Education/index.js
@@ -5,12 +5,171 @@ import './styles.css';
     // Relevant classes (projects / skills learned)
     // Relevant cerificates / online courses
 
+    // CLASSES TO INCLUDE:
+        // - FINA 408 - Financial Statements Analysis (in-depth analysis of companies' financial statements in Excel, culminating with a detailed valuation of Lululemon)
+        // - FINA 409 - Financial Modelling & Analysis (advanced use of Excel for financial modelling and analysis)
+        // - ECON 216 - Stats for Business & Economics (familiarity with statistics concepts and use of Excel for statistics and Tableau for visualizations)
+        // - BUAN 370 - Descriptive Analytics & Data Management (big data concepts, along as structuring, preprocessing, visualization and cleaning parts of the analytics process in R and SQL)
+        // - BUAN 371 - Analytical Decision Modelling (optimization techniques in Excel)
+        // - BUAN 390 - Business Analytics Strategy (visualization in Python and Tableau, Regression analysis and Machine Learning in Python)
+        // - ITMG 320 - Database Design and Business Intelligence Implementation (design and structuring database concepts and practice in SQL)
+        // - MGMT 497 Strategic Management (consulting, in-depth understanding of the decisions and strategies of businesses by analyzing case studies)
+        
+    
+    // CERTIFICATES / COURSES TO INCLUDE (check email for certificates of completion):
+        // - Udemy Tableau
+        //     - https://www.udemy.com/course/tableau10/
+        // - Udemy Statistics
+        //     - https://www.udemy.com/course/statistics-for-data-science-and-business-analysis/
+        // - Udemy MySQL
+        //     - https://www.udemy.com/course/the-ultimate-mysql-bootcamp-go-from-sql-beginner-to-expert/
+        // - Swirl R modules
+        // - Udemy Python
+        //     - https://www.udemy.com/course/the-modern-python3-bootcamp/
+        // - Udemy C/C++
+        //     - https://www.udemy.com/course/beginning-c-plus-plus-programming/
+        //     - https://github.com/guiOsorio/CPP-Practice
+        //     - https://github.com/guiOsorio/C-CPP-DataStructs
+        // - CS50
+        //     - https://cs50.harvard.edu/x/2022/syllabus/
+        //     - https://github.com/guiOsorio/CS50
+        // - Udemy WebDev
+        //     - https://www.udemy.com/course/the-web-developer-bootcamp/
+        // - Udemy Advanced CSS
+        //     - https://www.udemy.com/course/advanced-css-and-sass/
+
 class Education extends Component {
     state = {  } 
     render() { 
         return (
-            <div className="section__education screen-size">
+            <div className="section__education screen-size332">
                 <h2>EDUCATION</h2>
+                <div className='education-formal'>
+                    <h3>Formal Education</h3>
+                    <div className='education-formal__box'>
+                        <div className='box-left'>
+
+                        </div>
+                        <div className='box-right'>
+                            <h5 className='education__box-title'>BUSINESS ANALYTICS</h5>
+                            <div className='education__box-text'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='education-formal__box'>
+                        <div className='box-left'>
+
+                        </div>
+                        <div className='box-right'>
+                            <h5 className='education__box-title'>FINANCE</h5>
+                            <div className='education__box-text'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='education-formal__box'>
+                        <div className='box-left'>
+
+                        </div>
+                        <div className='box-right'>
+                            <h5 className='education__box-title'>OTHER NOTEWORTHY</h5>
+                            <div className='education__box-text'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='education-informal'>
+                    <h3>Certificates / Additional Courses</h3>
+                    <div className='education-informal__box'>
+                        <div className='box-left'>
+
+                        </div>
+                        <div className='box-right'>
+                            <h5 className='education__box-title'>ANALYTICS SKILLS</h5>
+                            <div className='education__box-text'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='education-informal__box'>
+                        <div className='box-left'>
+
+                        </div>
+                        <div className='box-right'>
+                            <h5 className='education__box-title'>COMPUTER SCIENCE CONCEPTS / TECHNOLOGIES</h5>
+                            <div className='education__box-text'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='education-informal__box'>
+                        <div className='box-left'>
+
+                        </div>
+                        <div className='box-right'>
+                            <h5 className='education__box-title'>WEB DEVELOPMENT</h5>
+                            <div className='education__box-text'>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
